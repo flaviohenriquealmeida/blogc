4e8faaafff6ea65aa3d1de44dbd16c225df7bb48
= <flaviohenriquealmeida@gmail.com>
2017-02-14 11:44:30 -0200

    missing index.js

---
+++ app/post/views/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    form: require('./form.marko'),
+    notfound: require('./notfound.marko'),
+    postView: require('./post-view.marko'),
+    posts: require('./posts.marko')
+}
