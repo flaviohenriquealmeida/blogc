40526a96b3a830d31827b066a56ae297cdab495f
= <flaviohenriquealmeida@gmail.com>
2017-03-16 17:48:51 -0300

    webpack-dev-server working

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,13 @@ module.exports = {
     
     devServer: {
         proxy: {
-            "/*": {
+            "**": {
                 target: "http://localhost:3000",
                 secure: false,
+                changeOrigin: true,
                 rewrite: function(req, options) {}
             },
-        }
+        }   
     },    
 }
 
